5714e528d53d07f3710d6ccf52b0b995ce79a53f
kkirayu <kukicha76@gmai.com>
2025-03-14 18:18:14 +0700

    Add pointer

--- cmake-build-debug/CMakeFiles/prak2.dir/main.cpp.o.ddi.i
+++ cmake-build-debug/CMakeFiles/prak2.dir/main.cpp.o.ddi.i
@@ -43299,7 +43299,39 @@ namespace std __attribute__ ((__visibility__ ("default")))
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
-    return 0;
+    int x = 100, y = 130, z;
 
+    int *ptrX, *ptrY;
+
+    cout << "Nilai x = " << x << ", y = " << y << endl;
+
+    cout << "Alamat x = " << &x << ", y = " << &y << endl;
+
+    ptrX = &x;
+
+    cout << "Isi ptrX = " << ptrX << " (alamat x), Nilai yg ditunjuk ptrX = " << *ptrX << endl;
+
+    ptrY = &y;
+
+    cout << "Isi ptrY = " << ptrY << " (alamat y), Nilai yg ditunjuk ptrY = " << *ptrY << endl;
+
+    z = *ptrX;
+
+    cout << "Nilai z = " << z << " Sama dengan nilai x (" << x << ")" << endl;
+
+    *ptrY = 70;
+
+    cout << "Isi ptrY = " << ptrY << " (tetap), nilai y = " << y << " (yg berubah)" << endl;
+
+    *ptrX = *ptrY + 5;
+
+    cout << "Isi ptrX = " << ptrX << " (tetap), nilai x = " << x << " (yg berubah)" << endl;
+
+    ptrX = ptrY;
+    cout << "Isi ptrX = " << ptrX << " dan ptrY = " << ptrY << endl;
+
+    cout << "Nilai yang ditunjuk ptrX = " << *ptrX << " dan ptrY = " << *ptrY << endl;
+
+    cout << endl;
+    return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,40 @@
 #include <iostream>
 using namespace std;
 
-    struct {
-        bool jk;
-        char name;
-        int age;
-        int height;
-        int weight;
+int main() {
+    int x = 100, y = 130, z;
 
-    };
+    int *ptrX, *ptrY;
 
-    int main() {
+    cout << "Nilai x = " << x << ", y = " << y << endl;
 
-    }
+    cout << "Alamat x = " << &x << ", y = " << &y << endl;
+
+    ptrX = &x;
+
+    cout << "Isi ptrX = " << ptrX << " (alamat x), Nilai yg ditunjuk ptrX = " << *ptrX << endl;
+
+    ptrY = &y;
+
+    cout << "Isi ptrY = " << ptrY << " (alamat y), Nilai yg ditunjuk ptrY = " << *ptrY << endl;
+
+    z = *ptrX;
+
+    cout << "Nilai z = " << z << " Sama dengan nilai x (" << x << ")" << endl;
+
+    *ptrY = 70;
+
+    cout << "Isi ptrY = " << ptrY << " (tetap), nilai y = " << y << " (yg berubah)" << endl;
+
+    *ptrX = *ptrY + 5;
+
+    cout << "Isi ptrX = " << ptrX << " (tetap), nilai x = " << x << " (yg berubah)" << endl;
+
+    ptrX = ptrY;
+    cout << "Isi ptrX = " << ptrX << " dan ptrY = " << ptrY << endl;
+
+    cout << "Nilai yang ditunjuk ptrX = " << *ptrX << " dan ptrY = " << *ptrY << endl;
+
+    cout << endl;
+    return 0;
+}
