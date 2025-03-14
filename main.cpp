8db4252d2fd0225dfa8fd0083db35dc7a706cbd5
kkirayu <kukicha76@gmai.com>
2025-03-14 07:53:04 +0700

    First commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+    struct {
+        bool jk;
+        char name;
+        int age;
+        int height;
+        int weight;
+
+    };
+
+    int main() {
+
+    }
